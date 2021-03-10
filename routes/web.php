0e46992064d651f826daadca7553817ea418cc41
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-03-09 20:24:27 -0400

    CRUD Routes publications created Routes for Comments created

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicationController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/publications', [PublicationController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('/publications/create', [PublicationController::class, 'create'])->middleware(['auth'])->name('publication.create');
+Route::post('/publications', [PublicationController::class, 'store'])->middleware(['auth'])->name('publication.store');
+Route::get('/publications/{publication}', [PublicationController::class, 'show'])->middleware(['auth'])->name('publication.show');
+Route::get('/publications/{publication}/edit', [PublicationController::class, 'edit'])->middleware(['auth'])->name('publication.edit');
+Route::patch('/publications/{publication}', [PublicationController::class, 'update'])->middleware(['auth'])->name('publication.update');
+Route::delete('/publications/{publication}', [PublicationController::class, 'destroy'])->middleware(['auth'])->name('publication.destroy');
 
+Route::patch('/comments/{comment}', [CommentController::class, 'approveComment'])->middleware(['auth'])->name('comment.approve');
+Route::post('/comments', [CommentController::class, 'store'])->middleware(['auth'])->name('comment.store');
 require __DIR__.'/auth.php';
