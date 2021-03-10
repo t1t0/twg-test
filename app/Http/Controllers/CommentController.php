dda5c7c15b20a014434d61ec97b0e116db30aa05
Viktor Rodriguez <viktor.a.rodriguez.o@gmail.com>
2021-03-09 20:24:49 -0400

    Publication Controller Created Comment Controller created

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Publication;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\newCommentMail;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request){
+        $validate = $request->validate([
+            'comment' => ['required']
+        ]);
+
+        $comment = Comment::create([
+            'user_id' => Auth::user()->id,
+            'content' => $request->comment,
+            'publication_id' => $request->post
+        ]);
+
+        $comment->save();
+
+        $publication = Publication::find($request->post);
+        Mail::to($publication->publishedBy->email)->send(new newCommentMail($comment->commentedTo->title, $comment->commentedBy->name, $comment->content));
+
+        return back();
+    }
+
+    public function approveComment(Comment $comment){
+        $comment->approve();
+        return back();
+    }
 }
--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -2,9 +2,111 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Publication;
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PublicationController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(){
+        $publications = Publication::all();
+        return view('publications.index', ['publications' => $publications]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('publications.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'title' => ['required', 'max:255'],
+            'content' => ['required']
+        ]);
+
+        $post = Publication::create([
+            'user_id' => Auth::user()->id,
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        $post->save();
+
+        return redirect()->route('dashboard');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Publication $publication)
+    {
+        $comments = Comment::where('user_id', Auth::user()->id)->where('publication_id', $publication->id)->count();
+        return view('publications.show', ['post' => $publication, 'comments' => $comments]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Publication $publication)
+    {
+        return view('publications.edit', ['post' => $publication]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Publication $publication)
+    {
+        $validate = $request->validate([
+            'title' => ['required', 'max:255'],
+            'content' => ['required']
+        ]);
+
+        $publication->title = $request->title;
+        $publication->content = $request->content;
+
+        $publication->save();
+
+        return redirect()->route('dashboard');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Publication  $publication
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Publication $publication)
+    {
+        $publication->delete();
+        return back();
+    }
 }
